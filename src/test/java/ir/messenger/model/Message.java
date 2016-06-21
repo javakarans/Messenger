486c6cab0333f38c99a3baa5c87dc66b784a2c78
javakarans <javakarans@gmail.com>
2016-06-21 18:32:35 +0430

    اضافه کردن فیلد به message

--- src/test/java/ir/messenger/model/Message.java
+++ src/test/java/ir/messenger/model/Message.java
@@ -1,7 +1,33 @@
 package ir.messenger.model;
 
-/**
- * Created by Mohammad on 6/20/2016.
- */
+
 public class Message {
+
+    private String senderID;
+    private String receiverID;
+    private String text;
+
+    public String getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(String senderID) {
+        this.senderID = senderID;
+    }
+
+    public String getReceiverID() {
+        return receiverID;
+    }
+
+    public void setReceiverID(String receiverID) {
+        this.receiverID = receiverID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
