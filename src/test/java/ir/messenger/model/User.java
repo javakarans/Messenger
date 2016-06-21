fd8611b09cbf2303101fbfc4bcef6882a605e85d
javakarans <javakarans@gmail.com>
2016-06-21 18:28:05 +0430

    اضافه کردن فیلدهای user

--- src/test/java/ir/messenger/model/User.java
+++ src/test/java/ir/messenger/model/User.java
@@ -1,7 +1,60 @@
 package ir.messenger.model;
 
-/**
- * Created by Mohammad on 6/20/2016.
- */
+
 public class User {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String pass;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
